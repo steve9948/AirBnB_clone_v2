a2e99743505b4e1795383f8a855ae40209c2c8d2
steve9948 <mbuguagichimu@gmail.com>
2024-01-17 11:19:48 -0800

    Added a state methd

--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class initial for the project"""
+
+    __tablename__ = 'states'
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade="all,delete", backref="state")
+    else:
+        name = ""
+        # FileStorage: getter attribute cities that
+        # returns the list of City instances with state_id equals
+
+        @property
+        def cities(self):
+            """getter document"""
+            from models import storage
+            citiesList = []
+            citiesAll = storage.all(City)
+            #iterate over to update details
+            for city in citiesAll.values():
+                if city.state_id == self.id:
+                    citiesList.append(city)
+            return citiesList
