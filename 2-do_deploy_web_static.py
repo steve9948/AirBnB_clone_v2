9ab000c76b61c4075a63a4f1967d258512357722
steve9948 <mbuguagichimu@gmail.com>
2024-02-01 04:54:12 -0800

    Deploying to webstatic

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" module doc
+"""
+from fabric.api import task, local, env, put, run
+from datetime import datetime
+import os
+
+
+env.hosts = ["18.209.224.156", "35.175.105.236"]
+
+
+def do_deploy(archive_path):
+    """Fabric script that distributes
+    an archive to your web server"""
+
+    if not path.exists(archive_path):
+        return False
+    try:
+        tgzfile = archive_path.split("/")[-1]
+        print(tgzfile)
+        filename = tgzfile.split(".")[0]
+        print(filename)
+        pathname = "/data/web_static/releases/" + filename
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(tgzfile, filename))
+        run("rm /tmp/{}".format(tgzfile))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
+        return True
+    except Exception as e:
+        return False
