5f7a9b9587878d925c9a8f9659b6362e2171470f
steve9948 <mbuguagichimu@gmail.com>
2024-02-01 01:48:50 -0800

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" A fabric file that generates a .tgz archive file
+"""
+from fabric.api import local
+from time import strftime
+from datetime import date
+
+
+@task
+def do_pack():
+    """ method doc
+        sudo fab -f 1-pack_web_static.py do_pack
+    """
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
+    return None
