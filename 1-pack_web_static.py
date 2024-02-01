41411464794360fbce7a83aeff905e03a48f421c
steve9948 <mbuguagichimu@gmail.com>
2024-02-01 02:24:53 -0800

    Created an archieve file for web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" A fabric file that generates a .tgz archive file
+""" module doc
 """
-from fabric.api import local
-from time import strftime
-from datetime import date
+from fabric.api import task, local
+from datetime import datetime
 
 
-@task
 def do_pack():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
+    """ A script that generates archive the contents of web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
+        return None
