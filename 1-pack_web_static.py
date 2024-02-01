ed7e5d5564f1b6d7a9b7ee753d06003c0d3e0e43
steve9948 <mbuguagichimu@gmail.com>
2024-02-01 03:17:23 -0800

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" module doc
-"""
 from fabric.api import task, local
 from datetime import datetime
 
 
+@task
 def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
-
-    filename = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
-
-        return "versions/web_static_{}.tgz".format(filename)
-
-    except Exception as e:
-        return None
+    """ method doc
+        sudo fab -f 1-pack_web_static.py do_pack
+    """
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
+    return None
