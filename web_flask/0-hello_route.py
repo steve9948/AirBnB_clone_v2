fd18a8ce244aea9e21bb74794967da89b687b932
steve9948 <mbuguagichimu@gmail.com>
2024-03-13 16:36:08 +0000

    Set up development with Python

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello():
-    """ def doc """
+    """ dispalay hello HBNB"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
+    """Start flask development server"""
     app.run(host="0.0.0.0", port=5000)
